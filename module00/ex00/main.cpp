e506a7866e7eed930ed4e2d5c8697cc96736f341
viodid <david.yunta.aller@gmail.com>
2025-01-16 11:56:01 +0100

    ex00

--- module00/ex00/main.cpp
+++ module00/ex00/main.cpp
@@ -1,7 +1,42 @@
 #include <iostream>
+#include <string>
 
-int	main()
+static std::string	capitalize_str(std::string str);
+static std::string	aggregate_args(int argc, char *argv[]);
+
+int	main(int argc, char *argv[])
 {
-	std::cout << "hello";
+	if (argc <= 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+		return (0);
+	}
+	const std::string str = aggregate_args(argc, argv);
+	std::cout << capitalize_str(str) << '\n';
 	return (0);
+}
+
+static std::string	aggregate_args(const int argc, char *argv[])
+{
+	std::string output;
+
+	int	i = 1;
+	while (i < argc)
+	{
+		output += argv[i];
+		i++;
+	}
+	return (output);
+}
+
+static std::string	capitalize_str(std::string str)
+{
+	int	i = 0;
+	while (str[i])
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] = static_cast<char>(str[i] - 32);
+		i++;
+	}
+	return (str);
 }
