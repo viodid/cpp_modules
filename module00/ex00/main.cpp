0a2408df8bac06a40ddaa3c562183f0607b307a8
viodid <david.yunta.aller@gmail.com>
2025-01-23 11:17:05 +0100

    correct ex00 use toUpper and reference str

--- module00/ex00/main.cpp
+++ module00/ex00/main.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
 #include <string>
+#include <cctype>
 
-static std::string	capitalize_str(std::string str);
+static std::string	capitalize_str(const std::string& str);
 static std::string	aggregate_args(int argc, char *argv[]);
 
 int	main(int argc, char *argv[])
 {
 	if (argc <= 1)
 	{
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 		return (0);
 	}
 	const std::string str = aggregate_args(argc, argv);
-	std::cout << capitalize_str(str) << '\n';
+	capitalize_str(str);
 	return (0);
 }
 
@@ -29,14 +30,14 @@ static std::string	aggregate_args(const int argc, char *argv[])
 	return (output);
 }
 
-static std::string	capitalize_str(std::string str)
+static std::string	capitalize_str(const std::string& str)
 {
 	int	i = 0;
 	while (str[i])
 	{
-		if (str[i] >= 'a' && str[i] <= 'z')
-			str[i] = static_cast<char>(str[i] - 32);
+		std::cout << static_cast<char>(std::toupper(str[i]));
 		i++;
 	}
+	std::cout << std::endl;
 	return (str);
 }
