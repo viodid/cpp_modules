959912e61c5fead999d500f1fb6ccf9f6277b2c5
viodid <david.yunta.aller@gmail.com>
2025-01-28 12:04:07 +0100

    main file filter input keyword

--- module00/ex01/src/main.cpp
+++ module00/ex01/src/main.cpp
@@ -1,10 +1,40 @@
-#include <iostream>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dyunta <dyunta@student.42madrid.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/14 19:47:26 by dyunta            #+#    #+#             */
+/*   Updated: 2025/01/07 17:03:07 by dyunta           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-int	main()
+#include <iostream>
+#include "../include/PhoneBook.hpp"
+
+int main()
 {
-	std::string n;
-	std::cout << "Test input: ";
-	std::cin >> n;
-	std::cout << "The input is: " << n << '\n';
+	PhoneBook phone_book;
+	std::string cmd;
+
+	while (!std::cin.eof())
+	{
+		std::cout << "Enter command [ADD, SEARCH, EXIT]: ";
+		std::getline(std::cin, cmd);
+
+		if (cmd == "ADD")
+			phone_book.add_contact();
+		else if (cmd == "SEARCH")
+		{
+			phone_book.display_contacts();
+		}
+		else if (cmd == "EXIT")
+			break;
+		else
+		{
+			std::cout << "Invalid command." << std::endl;
+		}
+	}
 	return (0);
 }
