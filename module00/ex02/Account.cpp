f6c008379bcec06294eea2861a64e4a3cdde3fba
viodid <david.yunta.aller@gmail.com>
2025-01-29 14:12:23 +0100

    initialize static Account member variables, displaytimestamp

--- module00/ex02/Account.cpp
+++ module00/ex02/Account.cpp
@@ -3,32 +3,42 @@
 #include <iostream>
 #include <ctime>
 
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
+
 Account::Account(const int initial_deposit) :
-	_accountIndex(0),
+	_accountIndex(_nbAccounts++),
 	_amount(initial_deposit),
 	_nbDeposits(0),
 	_nbWithdrawals(0)
 {
+	_totalAmount += _amount;
+	std::cout << "index:" << _accountIndex << ";";
+	std::cout << "amount:" << _amount << ";";
+	std::cout << "created" << std::endl;
 }
 
-int Account::getNbAccounts()
+Account::~Account()
 {
-	return _nbAccounts;
 }
 
-int Account::getTotalAmount()
-{
-	return _totalAmount;
-}
 
-int Account::getNbDeposits()
-{
-	return _totalNbDeposits;
-}
+int Account::getNbAccounts() { return _nbAccounts; }
 
-int Account::getNbWithdrawals()
+int Account::getTotalAmount() { return _totalAmount; }
+
+int Account::getNbDeposits() { return _totalNbDeposits; }
+
+int Account::getNbWithdrawals() { return _totalNbWithdrawals; }
+
+void Account::displayAccountsInfos()
 {
-	return _totalNbWithdrawals;
+	std::cout << "accounts:" << getNbAccounts() << ";";
+	std::cout << "total:" << getTotalAmount() << ";";
+	std::cout << "deposits:" << getNbDeposits() << ";";
+	std::cout << "withdrawals:" << getNbWithdrawals() << std::endl;
 }
 
 void Account::makeDeposit(const int deposit)
@@ -56,25 +66,16 @@ void Account::displayStatus() const
 	std::cout << "hey there";
 }
 
-
-
-void Account::displayAccountsInfos()
+void Account::_displayTimestamp()
 {
-	std::cout << "accounts:" << getNbAccounts() << ";";
-	std::cout << "total:" << getTotalAmount() << ";";
-	std::cout << "deposits:" << getNbDeposits() << ";";
-	std::cout << "withdrawals:" << getNbWithdrawals() << std::endl;
-}
-
-void	Account::_displayTimestamp() {
 	const std::time_t timestamp = std::time(NULL);
-	const std::tm *now = std::localtime(&timestamp);
+	const std::tm* now = std::localtime(&timestamp);
 
-	std::cout 	<< "[" << (now->tm_year + 1900)
-				<< (now->tm_mon + 1 < 10 ? "0" : "") << now->tm_mon + 1
-				<< (now->tm_mday < 10 ? "0" : "") << now->tm_mday + 1
-				<< "_" << (now->tm_hour < 10 ? "0" : "") << now->tm_hour
-				<< (now->tm_min < 10 ? "0" : "") << now->tm_min
-				<< (now->tm_sec < 10 ? "0" : "") << now->tm_sec
-				<< "] ";
+	std::cout << "[" << (now->tm_year + 1900)
+		<< (now->tm_mon + 1 < 10 ? "0" : "") << now->tm_mon + 1
+		<< (now->tm_mday < 10 ? "0" : "") << now->tm_mday + 1
+		<< "_" << (now->tm_hour < 10 ? "0" : "") << now->tm_hour
+		<< (now->tm_min < 10 ? "0" : "") << now->tm_min
+		<< (now->tm_sec < 10 ? "0" : "") << now->tm_sec
+		<< "] ";
 }
