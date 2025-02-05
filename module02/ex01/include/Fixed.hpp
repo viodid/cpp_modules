8989e64b04110b2a2bf86da0f4e478f31d7faac7
viodid <david.yunta.aller@gmail.com>
2025-02-05 14:31:08 +0100

    module02 ex01 boilerplate

---
+++ module02/ex01/include/Fixed.hpp
@@ -0,0 +1,22 @@
+#ifndef FIXED_H
+# define FIXED_H
+
+#include <iostream>
+
+class Fixed
+{
+public:
+	Fixed();
+	Fixed(Fixed& copy);
+	Fixed& operator=(Fixed& copy);
+	~Fixed();
+
+	int getRawBits();
+	void setRawBits(int raw);
+
+private:
+	int _value;
+	static const int _fracBits = 8;
+};
+
+#endif
