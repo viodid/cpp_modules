152ff36019ba1273fb63daf079f8d40bf91e513b
viodid <david.yunta.aller@gmail.com>
2025-02-11 16:31:28 +0100

    calculate cross product returning floats so it does not lose precision

--- module02/ex03/src/bsp.cpp
+++ module02/ex03/src/bsp.cpp
@@ -1,12 +1,12 @@
 #include "../include/Point.hpp"
 
-int cross_product(const Point& a, const Point& b, const Point& p);
+static float cross_product(const Point& a, const Point& b, const Point& p);
 
 bool bsp( Point const a, Point const b, Point const c, Point const point)
 {
-	const int d1 = cross_product(a, b, point);
-	const int d2 = cross_product(b, c, point);
-	const int d3 = cross_product(c, a, point);
+	const float d1 = cross_product(a, b, point);
+	const float d2 = cross_product(b, c, point);
+	const float d3 = cross_product(c, a, point);
 
 	if ((d1 > 0 && d2 > 0 && d3 > 0)
 		|| (d1 < 0 && d2 < 0 && d3 < 0))
@@ -14,9 +14,8 @@ bool bsp( Point const a, Point const b, Point const c, Point const point)
 	return false;
 }
 
-// TODO: make it return a float so it does not lose precision
-int cross_product(const Point& a, const Point& b, const Point& p)
+static float cross_product(const Point& a, const Point& b, const Point& p)
 {
 	const Fixed r = (b.getX() - a.getX()) * (p.getY() - a.getY()) - (b.getY() - a.getY()) * (p.getX() - a.getX());
-	return (r.toInt());
+	return (r.toFloat());
 }
--- module02/ex03/src/main.cpp
+++ module02/ex03/src/main.cpp
@@ -2,9 +2,9 @@
 
 int main() {
     const Point a(0.0, 0.0);
-    const Point b(0.0, 1.0);
-    const Point c(1.0, 0.0);
-    const Point point(0.9, 0.0);
+    const Point b(0.0, 10.0);
+    const Point c(10.0, 0.0);
+    const Point point(3.0, 5.5);
 
     const bool inside = bsp(a, b, c, point);
 
