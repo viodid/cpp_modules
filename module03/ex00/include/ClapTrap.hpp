d2f8cf2be30dcd4ae94d7e8db823f59dab6f32eb
viodid <david.yunta.aller@gmail.com>
2025-02-11 17:19:21 +0100

    function methods claptrap implementation

--- module03/ex00/include/ClapTrap.hpp
+++ module03/ex00/include/ClapTrap.hpp
@@ -1,6 +1,8 @@
 #ifndef CLAPTRAP_H
 # define CLAPTRAP_H
 #include <string>
+#include <ostream>
+#include <iostream>
 
 class ClapTrap
 {
@@ -8,13 +10,13 @@ public:
 	ClapTrap(const std::string& name);
 	~ClapTrap();
 
-	void attack(const std::string& target);
+	void attack(const std::string& target) const;
 	void takeDamage(unsigned int amount);
 	void beRepaired(unsigned int amount);
 private:
 	const std::string _name;
 	int _hitPoints = 10;
-	const int _energyPoints = 10;
+	int _energyPoints = 10;
 	const int _attackDamage = 0;
 };
 
--- module03/ex00/src/ClapTrap.cpp
+++ module03/ex00/src/ClapTrap.cpp
@@ -1,7 +1,40 @@
 #include "../include/ClapTrap.hpp"
 
+
 ClapTrap::ClapTrap(const std::string& name) : _name(name)
 {
-
+	std::cout << "ClapTrap constructor called" << std::endl;
 }
+
+ClapTrap::~ClapTrap()
+{
+	std::cout << "ClapTrap destructor called" << std::endl;
+}
+
+void ClapTrap::attack(const std::string& target) const
+{
+	std::cout << "ClapTrap " << _name << " attacks " << target << ", causing ";
+	std::cout << _attackDamage << " points of damage!" << std::endl;
+}
+
+void ClapTrap::takeDamage(const unsigned int amount)
+{
+	_hitPoints -= amount;
+	if (_hitPoints < 0)
+		_hitPoints = 0;
+	std::cout << "ClapTrap " << _name << " has been attacked, receiving ";
+	std::cout << amount << " points of damage!" << std::endl;
+
+	std::cout << "ClapTrap now has " << _hitPoints << "heal points.";
+}
+
+void ClapTrap::beRepaired(unsigned int amount)
+{
+	_hitPoints += amount;
+	std::cout << "ClapTrap " << _name << " has been repaired " << amount << " points!" << std::endl;
+
+	std::cout << "ClapTrap now has " << _hitPoints << "heal points.";
+}
+
+
 
