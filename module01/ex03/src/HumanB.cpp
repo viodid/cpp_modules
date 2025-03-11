8eeb82c2b576a49fb0dc7c7acd820d58364b4bdb
David Yunta <david.yunta.aller@gmail.com>
2025-03-11 20:17:38 +0100

    handle null reference

--- module01/ex03/src/HumanB.cpp
+++ module01/ex03/src/HumanB.cpp
@@ -10,7 +10,12 @@ HumanB::~HumanB()
 
 void HumanB::attack()
 {
+    if (!_weapon) {
+        std::cout << "There is no weapon! :O" << std::endl;
+        return ;
+    }
+
 	std::cout << _name << " attacks with their " << _weapon->getType() << std::endl;
 }
 
-void HumanB::setWeapon(Weapon& new_weapon) { _weapon = &new_weapon; }
+void HumanB::setWeapon(Weapon* new_weapon) { _weapon = new_weapon; }
