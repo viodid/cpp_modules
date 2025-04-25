9e3991e8f06ca1652c5df65d148c168e97d9331c
David Yunta <davidy@ext.inditex.com>
2025-04-25 11:22:36 +0200

    module03 - ex01: inherite fixes ex00, fix ort form

--- module03/ex01/src/ScavTrap.cpp
+++ module03/ex01/src/ScavTrap.cpp
@@ -1,54 +1,57 @@
 #include "../include/ScavTrap.hpp"
 
-ScavTrap::ScavTrap(const std::string& name) : ClapTrap(name)
+ScavTrap::ScavTrap()
+    : ClapTrap("default")
 {
-	std::cout << "ScavTrap constructor called" << std::endl;
-	_hitPoints = 100;
-	_energyPoints = 50;
-	_attackDamage = 20;
+    std::cout << "Scavtrap default constructor called" << std::endl;
 }
 
-ScavTrap::ScavTrap(const ScavTrap& obj) : ClapTrap(obj.getName())
+ScavTrap::ScavTrap(const std::string& name)
+    : ClapTrap(name)
 {
-	std::cout << "ScavTrap copy constructor called" << std::endl;
-	_hitPoints = ScavTrap.getHitPoints();
-	_energyPoints = ScavTrap.getEnergyPoints();
-	_attackDamage = ScavTrap.getAttackDamage();
+    std::cout << "ScavTrap constructor called" << std::endl;
+}
+
+ScavTrap::ScavTrap(const ScavTrap& obj)
+    : ClapTrap(obj)
+{
+    std::cout << "ScavTrap copy constructor called" << std::endl;
 }
 
 ScavTrap::~ScavTrap()
 {
-	std::cout << "ScavTrap destructor called" << std::endl;
+    std::cout << "ScavTrap destructor called" << std::endl;
 }
 
-ScavTrap& ScavTrap::operator=(const ScavTrap& obj) : ClapTrap(obj.getName())
+ScavTrap& ScavTrap::operator=(const ScavTrap& obj)
 {
     std::cout << "ScavTrap copy assignment operator called" << std::endl;
-	_hitPoints = ScavTrap.getHitPoints();
-	_energyPoints = ScavTrap.getEnergyPoints();
-	_attackDamage = ScavTrap.getAttackDamage();
+    if (this == &obj) {
+        _name = obj.getName();
+        _hitPoints = obj.getHitPoints();
+        _energyPoints = obj.getEnergPoints();
+        _attackDamage = obj.getAttackDamage();
+    }
+
     return *this;
 }
 
 void ScavTrap::attack(const std::string& target)
 {
-	if (_hitPoints <= 0)
-	{
-		std::cout << "ScavTrap does not have hit points, aborting!" << std::endl;
-		return;
-	}
-	if (_energyPoints <= 0)
-	{
-		std::cout << "ScavTrap does not have enough energy points!" << std::endl;
-		return;
-	}
-	_energyPoints -= 1;
-	std::cout << "ScavTrap " << _name << " attacks " << target << ", causing ";
-	std::cout << _attackDamage << " points of damage!" << std::endl;
+    if (getHitPoints() <= 0) {
+        std::cout << "ScavTrap does not have hit points, aborting!" << std::endl;
+        return;
+    }
+    if (getEnergPoints() <= 0) {
+        std::cout << "ScavTrap does not have enough energy points!" << std::endl;
+        return;
+    }
+    decreaseEnergy();
+    std::cout << "ScavTrap " << getName() << " attacks " << target << ", causing ";
+    std::cout << getAttackDamage() << " points of damage!" << std::endl;
 }
 
 void ScavTrap::guardGate() const
 {
-	std::cout << "ScavTrap " << _name << " is in guard mode!" << std::endl;
+    std::cout << "ScavTrap " << getName() << " is in guard mode!" << std::endl;
 }
-
--- module03/ex01/src/main.cpp
+++ module03/ex01/src/main.cpp
@@ -1,14 +1,18 @@
-#include "ScavTrap.hpp"
+#include "../include/ScavTrap.hpp"
 
-int main() {
+int main()
+{
 
-	ScavTrap anon("anon");
-	anon.attack("someone");
-	anon.beRepaired(3);
-	anon.takeDamage(12);
-	anon.attack("another one");
-	anon.takeDamage(1);
-	anon.beRepaired(10);
-	anon.guardGate();
-	return 0;
+    ScavTrap anon("anon");
+    ScavTrap Bob(anon);
+    Bob.guardGate();
+    Bob.attack("hey");
+    anon.attack("someone");
+    anon.beRepaired(3);
+    anon.takeDamage(12);
+    anon.attack("another one");
+    anon.takeDamage(1);
+    anon.beRepaired(10);
+    anon.guardGate();
+    return 0;
 }
