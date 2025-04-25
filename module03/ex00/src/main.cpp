f5fe0da4286fda17c5a8ef69f10d40bd9a680ff5
David Yunta <davidy@ext.inditex.com>
2025-04-25 11:19:44 +0200

    module03 - ex00: write all getters for priv att, fix ort form

--- module03/ex00/src/main.cpp
+++ module03/ex00/src/main.cpp
@@ -1,13 +1,15 @@
 #include "../include/ClapTrap.hpp"
 
-int main() {
-
-	ClapTrap anon("anon");
-	anon.attack("someone");
-	anon.beRepaired(3);
-	anon.takeDamage(12);
-	anon.attack("another one");
-	anon.takeDamage(1);
-	anon.beRepaired(10);
-	return 0;
+int main()
+{
+    ClapTrap anon("anon");
+    ClapTrap Bob(anon);
+    Bob.attack("hey");
+    anon.attack("someone");
+    anon.beRepaired(3);
+    anon.takeDamage(12);
+    anon.attack("another one");
+    anon.takeDamage(1);
+    anon.beRepaired(10);
+    return 0;
 }
