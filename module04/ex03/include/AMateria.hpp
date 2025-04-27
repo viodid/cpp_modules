df36e78137dcd5120158abb8f5f28912e2fa6900
viodid <david.yunta.aller@gmail.com>
2025-04-27 11:31:18 +0200

    module04 - ex03: clangd format

--- module04/ex03/include/AMateria.hpp
+++ module04/ex03/include/AMateria.hpp
@@ -1,26 +1,25 @@
 #ifndef AMATERIA_HPP
-# define AMATERIA_HPP
-# include <string>
-# include <ostream>
-# include <iostream>
+#define AMATERIA_HPP
+#include <iostream>
+#include <ostream>
+#include <string>
 
 class ICharacter;
 
-class AMateria
-{
+class AMateria {
 protected:
-	std::string _type;
-	AMateria();
-	AMateria(const AMateria& obj);
-	AMateria& operator=(const AMateria& obj);
+    std::string _type;
+    AMateria();
+    AMateria(const AMateria& obj);
+    AMateria& operator=(const AMateria& obj);
 
 public:
-	AMateria(const std::string& type);
-	virtual ~AMateria();
+    AMateria(const std::string& type);
+    virtual ~AMateria();
 
-	const std::string& getType() const;
-	virtual AMateria* clone() const = 0;
-	virtual void use(ICharacter& target);
+    const std::string& getType() const;
+    virtual AMateria* clone() const = 0;
+    virtual void use(ICharacter& target);
 };
 
 #endif
--- module04/ex03/include/Ice.hpp
+++ module04/ex03/include/Ice.hpp
@@ -1,18 +1,17 @@
 #ifndef ICE_HPP
-# define ICE_HPP
+#define ICE_HPP
 #include "AMateria.hpp"
 
-class Ice : public AMateria
-{
+class Ice : public AMateria {
 protected:
-	Ice(const Ice& obj);
-	Ice& operator=(const Ice& obj);
+    Ice(const Ice& obj);
+    Ice& operator=(const Ice& obj);
 
 public:
-	Ice();
-	~Ice();
-	AMateria* clone() const;
-	void use(ICharacter& target);
+    Ice();
+    ~Ice();
+    AMateria* clone() const;
+    void use(ICharacter& target);
 };
 
 #endif
