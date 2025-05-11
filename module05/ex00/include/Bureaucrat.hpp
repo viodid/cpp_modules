7c0083b610373d6d65a7b7a0642756243ce7c9ab
viodid <david.yunta.aller@gmail.com>
2025-05-11 11:01:53 +0200

    bureaucrat class

--- module05/ex00/include/Bureaucrat.hpp
+++ module05/ex00/include/Bureaucrat.hpp
@@ -1,9 +1,41 @@
 #ifndef BUREAUCRAT_H
 #define BUREAUCRAT_H
 
+#include <exception>
 #include <string>
 
 class Bureaucrat {
+private:
+    const std::string& _name;
+    uint _grade;
+
+public:
+    Bureaucrat();
+    Bureaucrat(const std::string& name);
+    Bureaucrat(const Bureaucrat& copy);
+    ~Bureaucrat();
+    Bureaucrat& operator=(const Bureaucrat& copy);
+
+    const std::string& getName();
+    uint getGrade();
+
+    class GradeTooHighException : public std::exception {
+    private:
+        std::string error;
+
+    public:
+        GradeTooHighException(std::string error);
+        const std::string& getError() const;
+    };
+
+    class GradeTooLowException : public std::exception {
+    private:
+        std::string error;
+
+    public:
+        GradeTooLowException(std::string error);
+        const std::string& getError() const;
+    };
 };
 
 #endif
