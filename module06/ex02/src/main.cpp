0c8524488379abbe16b50acc366ec008e1326f20
viodid <david.yunta.aller@gmail.com>
2025-05-29 12:11:12 +0200

    ex02 - main tests

--- module06/ex02/src/main.cpp
+++ module06/ex02/src/main.cpp
@@ -1,9 +1,18 @@
 #include "../include/Base.hpp"
 
+class D : public Base { };
+
 int main(void)
 {
     Base* base = generate();
-    std::cout << base << std::endl;
     identify(base);
+    Base& base_ref = *base;
+    identify(base_ref);
+    std::cout << "=== Wrong Class ===" << std::endl;
+    D wrong;
+    identify(&wrong);
+    Base& wrong_ref = wrong;
+    identify(wrong_ref);
+    delete base;
     return 0;
 }
