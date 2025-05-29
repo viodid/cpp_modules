a4ef2f08596defc6c3b1aab92e3b8fdfed743c2a
viodid <david.yunta.aller@gmail.com>
2025-05-29 12:31:25 +0200

    ex02 - identify reference

--- module06/ex02/src/Base.cpp
+++ module06/ex02/src/Base.cpp
@@ -37,3 +37,26 @@ void identify(Base* p)
     else
         std::cout << "Pointer doesn't correspond to any child class" << std::endl;
 }
+
+void identify(Base& p)
+{
+    try {
+        (void)dynamic_cast<A&>(p);
+        std::cout << "A" << std::endl;
+        return;
+    } catch (std::exception& e) {
+    }
+    try {
+        (void)dynamic_cast<B&>(p);
+        std::cout << "B" << std::endl;
+        return;
+    } catch (std::exception& e) {
+    }
+    try {
+        (void)dynamic_cast<C&>(p);
+        std::cout << "C" << std::endl;
+        return;
+    } catch (std::exception& e) {
+    }
+    std::cout << "Reference doesn't correspond to any child class" << std::endl;
+}
