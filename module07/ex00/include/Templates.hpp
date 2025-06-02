70befdd14a1f113cea1fb12439c3ba26aedc8953
viodid <david.yunta.aller@gmail.com>
2025-06-02 12:13:10 +0200

    module07 - ex00

---
+++ module07/ex00/include/Templates.hpp
@@ -0,0 +1,24 @@
+#ifndef TEMPLATE_H
+#define TEMPLATE_H
+
+template <typename T>
+void swap(T& a, T& b)
+{
+    T tmp = a;
+    a = b;
+    b = tmp;
+}
+
+template <typename T>
+T min(T a, T b)
+{
+    return (a < b ? a : b);
+}
+
+template <typename T>
+T max(T a, T b)
+{
+    return (a > b ? a : b);
+}
+
+#endif
