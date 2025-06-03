d412c3742c912a3c0fd25bfd2202f3eca50e73e7
viodid <david.yunta.aller@gmail.com>
2025-06-03 19:13:21 +0200

    ex02 - Array class constructors

--- module07/ex02/include/Array.hpp
+++ module07/ex02/include/Array.hpp
@@ -1,6 +1,18 @@
-#ifndef TEMPLATE_H
-#define TEMPLATE_H
+#ifndef ARRAY_H
+#define ARRAY_H
 
 template <typename T>
+class Array {
+private:
+    T _element;
+    int size;
+
+public:
+    Array();
+    Array(int n);
+    Array(const Array& cp);
+    ~Array();
+    Array& operator=(const Array& cp);
+};
 
 #endif
