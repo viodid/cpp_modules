9973d19fa5528ce972b977d87adf83cd04e75549
viodid <david.yunta.aller@gmail.com>
2025-06-12 11:29:52 +0200

    ex00 - main test fill container with rand num

--- module08/ex00/src/main.cpp
+++ module08/ex00/src/main.cpp
@@ -3,18 +3,24 @@
 #include <ostream>
 #include <vector>
 
+#define SIZE 10
+
 int main(void)
 {
     // TODO: use random and iterators to fill up the containers
     // Use 4 types of containers
     try {
+        srand(time(NULL));
+
         std::vector<int> v;
-        v.push_back(3);
-        v.push_back(2);
-        v.push_back(1);
-        v.push_back(42);
-        v.push_back(53);
-        std::cout << easyfind(v, 42).size() << std::endl;
+        for (int i = 0; i < SIZE; i++) {
+            int randN = rand() % SIZE;
+            std::cout << "index " << i + 1 << ":\t" << randN << std::endl;
+            v.push_back(randN);
+        }
+        int toBeFound = rand() % SIZE + 3;
+        std::cout << "to be found:\t" << toBeFound << std::endl;
+        std::cout << *easyfind(v, toBeFound) << std::endl;
     } catch (std::exception& e) {
         std::cout << e.what() << std::endl;
     }
