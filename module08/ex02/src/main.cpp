1c0600ab63859b51b3e912674a04df457c0cb5c1
viodid <david.yunta.aller@gmail.com>
2025-06-17 13:00:20 +0200

    ex02 - main tests

--- module08/ex02/src/main.cpp
+++ module08/ex02/src/main.cpp
@@ -1,34 +1,23 @@
 #include "../include/MutantStack.hpp"
 #include <iostream>
+#include <list>
 #include <ostream>
-#include <queue>
-
-template <typename T>
-static void tester(T& container);
 
 int main()
 {
+    std::cout << "== STACK ==" << std::endl;
     MutantStack<int> mstack;
-    tester(mstack);
-    std::queue<int> l;
-    tester(l);
-    return 0;
-}
-
-template <typename T>
-static void tester(T& container)
-{
-    container.push(5);
-    container.push(17);
-    std::cout << container.top() << std::endl;
-    container.pop();
-    std::cout << container.size() << std::endl;
-    container.push(3);
-    container.push(5);
-    container.push(737);
-    container.push(0);
-    MutantStack<int>::iterator it = container.begin();
-    MutantStack<int>::iterator ite = container.end();
+    mstack.push(5);
+    mstack.push(17);
+    std::cout << mstack.top() << std::endl;
+    mstack.pop();
+    std::cout << mstack.size() << std::endl;
+    mstack.push(3);
+    mstack.push(5);
+    mstack.push(737);
+    mstack.push(0);
+    MutantStack<int>::iterator it = mstack.begin();
+    MutantStack<int>::iterator ite = mstack.end();
     std::cout << "begin:\t" << *it << std::endl;
     std::cout << "end:\t" << *ite << std::endl;
     ++it;
@@ -37,4 +26,27 @@ static void tester(T& container)
         std::cout << *it << std::endl;
         ++it;
     }
+    std::stack<int> s(mstack);
+
+    std::cout << "== LIST ==" << std::endl;
+    std::list<int> l;
+    l.push_back(5);
+    l.push_back(17);
+    std::cout << l.back() << std::endl;
+    l.erase(--l.end());
+    std::cout << l.size() << std::endl;
+    l.push_back(3);
+    l.push_back(5);
+    l.push_back(737);
+    l.push_back(0);
+    std::list<int>::iterator lit = l.begin();
+    std::cout << "begin:\t" << *(l.begin()) << std::endl;
+    std::cout << "end:\t" << *(--l.end()) << std::endl;
+    ++lit;
+    --lit;
+    while (lit != l.end()) {
+        std::cout << *lit << std::endl;
+        ++lit;
+    }
+    return 0;
 }
