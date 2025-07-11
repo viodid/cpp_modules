ec2cb70e44909105bca64536d5f872e7bb7b9b43
viodid <david.yunta.aller@gmail.com>
2025-07-11 13:55:25 +0200

    ex01 - define helper structs and enum for tokenizer

--- module09/ex01/include/RPN.hpp
+++ module09/ex01/include/RPN.hpp
@@ -1,16 +1,34 @@
 #ifndef RPN_HPP
 #define RPN_HPP
 
-#define OPERATOR "+-/*"
+#define OPERATORS "+-/*"
 #define OPERANDS "0123456789"
 
 #include <ostream>
 #include <stack>
 #include <string>
 
+typedef enum e_tokenType {
+    OPERATOR,
+    OPERAND,
+} tokenType;
+
+typedef struct s_token {
+    tokenType type;
+    std::string literal;
+} t_token;
+
+typedef struct s_list {
+    void* content;
+    s_list* next;
+} t_list;
+
 class RPN {
 private:
     std::stack<float> _stack;
+
+    t_list* _tokenizer(const std::string& ex);
+    void _parseExpression(t_list* tokens);
 
 public:
     RPN();
@@ -19,7 +37,7 @@ public:
     RPN& operator=(const RPN& cp);
 
     const std::stack<float>& getStack() const;
-    void parseExpression(const std::string&);
+    void calculateExpression(const std::string&);
 
     class ExceptionOverflow : public std::exception {
         virtual const char* what() const throw();
